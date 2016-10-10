a8589d341d3aa208197106401182cfd341ebc18d
Kris Sankaran <sankaran.kris@gmail.com>
2016-10-10 14:39:58 -0700

    New parameters given fixes

--- rml-hmc/gaussian.R
+++ rml-hmc/gaussian.R
@@ -5,15 +5,6 @@
 
 source("gaussian_funs.R")
 
-x <- rnorm(30, 0, 10)
-res <- mcmc(x, c("mu" = 5, "sigma" = 40), .75, 200)
-mean(res$acceptances)
-colnames(res$thetas)
-plot(res$thetas[, 1])
-plot(res$thetas[, 2])
-plot(res$thetas, asp = 1, col = "white")
-lines(res$thetas)
-
 x <- rnorm(300, 0, 10)
 res <- mcmc(x, c("mu" = 5, "sigma" = 40), 0.4, 200)
 mean(res$acceptances)
@@ -23,10 +14,20 @@ plot(res$thetas[, 2])
 plot(res$thetas, col = "white", asp = 1)
 lines(res$thetas)
 
+x <- rnorm(300, 0, 10)
+res <- mcmc(x, c("mu" = 5, "sigma" = 40), 0.1, 2000)
+mean(res$acceptances)
+colnames(res$thetas)
+plot(res$thetas[, 1])
+plot(res$thetas[, 2])
+plot(res$thetas, col = "white", asp = 1)
+lines(res$thetas)
+
+
 samples_df <- data.frame(
   res$thetas,
   accept = res$acceptances,
   iter = seq_len(nrow(res$thetas))
 )
 
-save.image("~/Desktop/langevin_exper.Rdata")
+#save.image("~/Desktop/langevin_exper.Rdata")
