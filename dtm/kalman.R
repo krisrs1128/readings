8875dfca33d65ca4fe126e6f62e44c7c1ec0a00f
Kris Sankaran <sankaran.kris@gmail.com>
2016-10-18 18:41:03 -0700

    Kalman filtering implementation

---
+++ dtm/kalman.R
@@ -0,0 +1,78 @@
+
+###############################################################################
+# kalman filtering
+###############################################################################
+
+#' @title Kalman filtering
+#' @param A Evolution matrix across timepoints
+#' @param C Observation matrix across timepoints
+#' @examples
+#' p <- 10
+#' n_times <- 1000
+#' y <- topic_params(p, n_times)
+#' mu0 <- rnorm(p)
+#' sigma0 <- diag(p)
+#' A <- array(dim = c(p, p, n_times))
+#' C <- array(dim = c(p, p, n_times))
+#' Q <- array(dim = c(p, p, n_times))
+#' R <- array(dim = c(p, p, n_times))
+#' for (i in seq_len(n_times)) {
+#'   A[,,i] <- diag(p)
+#'   R[,,i] <- diag(p)
+#'   Q[,,i] <- diag(p)
+#'   C[,,i] <- diag(p)
+#' }
+#'
+#' y_tilde <- kalman_filter(y, mu0, sigma0, A, C, Q, R)
+#' plot(y[, 1], type = "l")
+#' lines(y_tilde$mu[, 1], col = "red")
+kalman_filter <- function(y, mu0, sigma0, A, C, Q, R) {
+  n_times <- nrow(y)
+  p <- ncol(y)
+
+  filtered_values <- list(
+    "mu" = matrix(nrow = n_times, ncol = p),
+    "sigma" = array(dim = c(p, p, n_times))
+  )
+
+  filtered_values$mu[1, ] <- mu0
+  filtered_values$sigma[,, 1] <- sigma0
+  for (i in seq_len(n_times - 1)) {
+    pred_step <- measurement_update(
+      filtered_values$mu[i, ],
+      filtered_values$sigma[,, i],
+      A[,, i + 1],
+      Q[,, i + 1]
+    )
+
+    cur_updates <- prediction_update(
+      y[i, ],
+      C[,, i + 1],
+      pred_step$mu_pred,
+      pred_step$sigma_pred,
+      R[,, i]
+    )
+
+    filtered_values$mu[i + 1, ] <- cur_updates$mu_cur
+    filtered_values$sigma[,, i + 1] <- cur_updates$sigma_cur
+  }
+
+  filtered_values
+}
+
+measurement_update <- function(mu_prev, sigma_prev, A_cur, Q_cur) {
+  list(
+    "mu_pred" = A_cur %*% mu_prev,
+    "sigma_pred" = A_cur %*% sigma_prev %*% t(A_cur) + Q_cur
+  )
+}
+
+prediction_update <- function(y_cur, C_cur, mu_pred, sigma_pred, R_cur) {
+  y_hat <- as.numeric(C_cur %*% mu_pred)
+  K_cur <- solve(solve(sigma_pred) + t(C_cur) %*% R_cur %*% C_cur) %*% t(C_cur) %*% solve(R_cur)
+
+  list(
+    "mu_cur" = mu_pred + K_cur %*% (y_cur - y_hat),
+    "sigma_cur" = (diag(nrow(sigma_pred)) - K_cur %*% C_cur) %*% sigma_pred
+  )
+}
