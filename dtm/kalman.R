2f45af644dc9410402ae548b70f64fd211cbafae
Kris Sankaran <sankaran.kris@gmail.com>
2016-10-18 19:07:06 -0700

    RTS smoother

--- dtm/kalman.R
+++ dtm/kalman.R
@@ -35,6 +35,11 @@ kalman_filter <- function(y, mu0, sigma0, A, C, Q, R) {
     "sigma" = array(dim = c(p, p, n_times))
   )
 
+  pred_values <- list(
+    "mu_pred" = matrix(nrow = n_times, ncol = p),
+    "sigma_pred" = array(dim = c(p, p, n_times))
+  )
+
   filtered_values$mu[1, ] <- mu0
   filtered_values$sigma[,, 1] <- sigma0
   for (i in seq_len(n_times - 1)) {
@@ -55,9 +60,15 @@ kalman_filter <- function(y, mu0, sigma0, A, C, Q, R) {
 
     filtered_values$mu[i + 1, ] <- cur_updates$mu_cur
     filtered_values$sigma[,, i + 1] <- cur_updates$sigma_cur
+    pred_values$mu[i + 1, ] <- pred_step$mu_pred
+    pred_values$sigma[,, i + 1] <- pred_step$sigma_pred
+
   }
 
-  filtered_values
+  list(
+    "filtered" = filtered_values,
+    "predicted" = pred_values
+  )
 }
 
 measurement_update <- function(mu_prev, sigma_prev, A_cur, Q_cur) {
@@ -74,5 +85,30 @@ prediction_update <- function(y_cur, C_cur, mu_pred, sigma_pred, R_cur) {
   list(
     "mu_cur" = mu_pred + K_cur %*% (y_cur - y_hat),
     "sigma_cur" = (diag(nrow(sigma_pred)) - K_cur %*% C_cur) %*% sigma_pred
+  )
+}
+
+smooth_filtered_estimates <- function(mu_filtered, sigma_filtered, mu_pred, sigma_pred, A) {
+  n_times <- nrow(mu_filtered)
+  p <- ncol(mu_filtered)
+
+  mu_smoothed <- matrix(nrow = n_times, ncol = p)
+  sigma_smoothed <- array(dim = dim(sigma_filtered))
+
+  mu_smoothed[n_times, ] <- mu_filtered[n_times, ]
+  sigma_smoothed[,, n_times] <- sigma_filtered[,, n_times]
+
+  for (i in (n_times - 1):1) {
+    J <- sigma_filtered[,, i] %*% t(A[,, i + 1]) %*% solve(sigma_pred[,, i + 1])
+
+    mu_smoothed[i, ] <- mu_filtered[i, ] +
+      J %*% (mu_smoothed[i + 1, ] - mu_pred[i + 1, ])
+    sigma_smoothed[,, i] <- sigma_filtered[,, i] +
+      J %*% (sigma_smoothed[,, i + 1] - sigma_pred[,, i + 1]) %*% t(J)
+  }
+
+  list(
+    "mu_smoothed" = mu_smoothed,
+    "sigma_smoothed" = sigma_smoothed
   )
 }
