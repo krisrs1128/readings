9e0de20d46ef55d51cb0192dc66c3ca54e4b659d
Kris Sankaran <sankaran.kris@gmail.com>
2016-11-21 09:57:01 -0800

    Alternative plots for counts data

--- lda/vb_exper.R
+++ lda/vb_exper.R
@@ -6,13 +6,35 @@ ndv <- dcast(lda_data$Nv, document ~ word, fill = 0) %>%
   select(-document) %>%
   as.matrix()
 
+row_order <-  hclust(dist(ndv))$order
+col_order <- hclust(dist(t(ndv)))$order
+
+ndv_plot <- data.frame(
+  "doc" = seq_len(nrow(ndv)),
+  "word" = ndv
+) %>%
+  melt(
+    id.vars = "doc",
+    variable.name = "word"
+  )
+
+ndv_plot$doc <- factor(ndv_plot$doc, levels = row_order)
+ndv_plot$word <- factor(
+  gsub("word\\.", "", ndv_plot$word),
+  levels = col_order
+)
+
+ggplot(ndv_plot) +
+  geom_tile(aes(x = doc, y = word, fill = value))
+
 res <- vb_counts(ndv, rep(1, 3), rep(1, 15), n_iter = 10)
+
 theta_tilde <- res$theta_tilde
 for(i in 1:nrow(theta_tilde)) {
   theta_tilde[i, ] <- theta_tilde[i, ] / sum(theta_tilde[i, ])
 }
 
-plot(sqrt(lda_data$theta[, 3]), sqrt(theta_tilde[, 1]), asp =1 )
+plot(sqrt(lda_data$theta[, 1]), sqrt(theta_tilde[, 1]), asp =1 )
 abline(b = 1, a = 0, col = 'red')
 
 beta_tilde <- res$beta_tilde
