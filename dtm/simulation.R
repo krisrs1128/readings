f538ea56e24b89bf5dea390e14b1a7d1fd5579f4
Kris Sankaran <sankaran.kris@gmail.com>
2016-11-29 11:41:24 -0800

    Visualize unigram estimates

--- dtm/simulation.R
+++ dtm/simulation.R
@@ -24,7 +24,6 @@ if (any(!.inst)) {
 # Load packages into session
 sapply(.packages, require, character.only = TRUE)
 set.seed(05122016)
-cat("\014")  # Clear console
 rm(list = ls()) # Delete all existing variables
 graphics.off() # Close all open plots
 
@@ -47,18 +46,19 @@ source("./unigram.R")
 
 # Code Block -------------------------------------------------------------------
 
-# generate data
-N <- 100
+## ---- generate_data ----
+N <- 2000
 V <- 15
-T <- N
-times <- 1:N
-times_mapping <- times
-sigma <- 0.1
+T <- N / 20
+times <- 1:T
+times_mapping <- rep(1:T, each = 20)
+sigma <- 1
 
-beta <- topic_params(V, N, sigma = sigma)
-X <- word_counts(beta, rep(500, N))
+beta <- topic_params(V, T, sigma = sigma)
+X <- word_counts(beta[times_mapping, ], rep(500, N))
 head(X)
 
+## ---- run_model ----
 stan_data <- list(
   N = N,
   V = V,
@@ -73,5 +73,25 @@ m <- stan_model("unigram.stan")
 stan_fit <- vb(m, data = stan_data)
 samples <- extract(stan_fit)
 
+## ---- visualize_beta ----
 beta_hat <- samples$beta %>%
-  apply(c(2, 3), mean)
+  apply(c(2, 3), mean) %>%
+  melt(varnames = c("sample", "word"))
+beta_sd <- samples$beta %>%
+  apply(c(2, 3), sd) %>%
+  melt(varnames = c("sample", "word"), value.name = "sd")
+beta_hat <- beta_hat %>%
+  left_join(beta_sd) %>%
+  mutate(type = "estimate")
+
+mbeta <- beta %>%
+  melt(varnames = c("sample", "word")) %>%
+  mutate(type = "truth", sd = NA)
+mbeta <- rbind(mbeta, beta_hat)
+
+ggplot(mbeta) +
+  geom_line(aes(x = sample, y = value, col = type)) +
+  geom_line(aes(x = sample, y = value - 1.96 * sd, col = type), linetype = 2) +
+  geom_line(aes(x = sample, y = value + 1.96 * sd, col = type), linetype = 2) +
+  scale_color_brewer(palette = "Set2") +
+  facet_wrap(~word)
