7585151c907ddeadf458bc7c5b2dd568acf3a459
Kris Sankaran <sankaran.kris@gmail.com>
2017-02-22 14:47:59 -0800

    Function to simulate ARCH process

--- inhomog-unigram/simulate.R
+++ inhomog-unigram/simulate.R
@@ -22,21 +22,61 @@
 #' mus <- gaussian_rw(mu0, ts)
 #'
 #' ## changepoint in variances
-#' sigmas <- c(rep(1, 20), rep(4, 5), rep(1, 25))
+#' sigmas <- c(rep(1, 20), rep(6, 10), rep(1, 19))
 #' mus <- gaussian_rw(mu0, ts, sigmas)
 #' plot(ts, mus[, 1])
+#'
 gaussian_rw <- function(mu0, ts, sigmas = NULL) {
   n_times <- length(ts)
-  V <- length(mu0)
-  mus <- matrix(0, n_times, V)
+  p <- length(mu0)
+  deltas <- matrix(
+    rnorm((n_times - 1) * p),
+    n_times - 1,
+    p
+  )
 
   if (is.null(sigmas)) {
     sigmas <- rep(1, n_times - 1)
   }
-  print(sigmas)
 
-  for (i in seq_along(ts)[-1]) {
-    mus[i, ] <- sigmas[i - 1] * sqrt(ts[i] - ts[i - 1]) * rnorm(V, mean = mus[i - 1, ])
+  for (i in seq_len(nrow(deltas))) {
+    deltas[i, ] <- sqrt(ts[i + 1] - ts[i]) * sigmas[i] * deltas[i, ]
   }
-  mus
+
+  rw(mu0, deltas)
 }
+
+rw <- function(x0, deltas) {
+  n_times <- 1 + nrow(deltas)
+  p <- ncol(deltas)
+  x <- matrix(0, n_times, p)
+
+  for (i in seq_len(n_times)[-1]) {
+    x[i, ]  <- x[i - 1] + deltas[i - 1, ]
+  }
+
+  x
+}
+
+#' Order 1 ARCH
+#'
+#' @param ts [length n_times vector] A sorted vector of times
+#' @param a0 [scalar] The initial value of the arch process
+#' @param alpha_0 [scalar] The baseline noise in the ARCH
+#' @param alpha_1 [scalar] How strongly the variances persist across timepoints
+#'
+#' @examples
+#' mus <- arch(ts, alpha_0 = 1, alpha_1 = 0.95)
+#' plot(ts, mus)
+arch <- function(ts, a0 = 0.1, alpha_0 = 1, alpha_1 = 0.7) {
+  n_times <- length(ts)
+  eps_sq <- rnorm(n_times - 1) ^ 2
+  a_sq <- c(a0 ^ 2, rep(NA, n_times - 1))
+
+  for (i in seq_along(a_sq)[-1]) {
+    a_sq[i] <- (alpha_0 + alpha_1 * a_sq[i - 1]) * (ts[i] - ts[i - 1]) * eps_sq[i - 1]
+  }
+
+  sqrt(a_sq)
+}
+
