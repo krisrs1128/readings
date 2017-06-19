7dd3afabc654b0f530f466aa4bf0b08b9de718e5
Kris Sankaran <sankaran.kris@gmail.com>
2017-06-19 13:54:54 -0700

    empirical transition probabilities

---
+++ sticky_hdp_hmm/direct_sampler.R
@@ -0,0 +1,22 @@
+#! /usr/bin/env Rscript
+
+## File description -------------------------------------------------------------
+## Code for the direct assignment collapsed gibbs sampler for the sticky
+## HDP-HMM, as described in Algorithm 8 of Emily Fox's thesis
+##
+## author: Kris Sankaran
+## date: 06/19/2017
+
+#' @examples
+#' z <- c(1, 1, 2, 1, 1, 3, 3, 3, 1)
+#' transition_counts(z)
+transition_counts <- function(z) {
+  K <- max(z)
+  n <- matrix(0, nrow = K, ncol = K)
+  time_len <- length(z)
+
+  for (i in seq_len(time_len - 1)) {
+    n[z[i], z[i + 1]] <- n[z[i], z[i + 1]] + 1
+  }
+  n
+}
