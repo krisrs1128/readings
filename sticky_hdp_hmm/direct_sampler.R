df80f955fe2c545befaf2e47d5e809b5319ba7bc
Kris Sankaran <sankaran.kris@gmail.com>
2017-06-19 14:22:49 -0700

    function to retrieve prior predictive zt given rest

--- sticky_hdp_hmm/direct_sampler.R
+++ sticky_hdp_hmm/direct_sampler.R
@@ -35,13 +35,24 @@ increment_count <- function(n, z_prev, z_cur, z_next) {
   n[z_cur, z_next] <- n[z_cur, z_next] + 1
 }
 
-f_vector <- function(yt, z_prev, z_next, n, alpha, beta, kappa, theta) {
+prior_predictive <- function(z_prev, z_next, n, alpha, beta, kappa) {
   K <- nrow(n)
-  f <- vector(length = K)
+  pzt <- vector(length = K + 1)
 
+  ## equation (A10, page 216)
   for (k in seq_len(K)) {
-    f[k] <- (alpha * beta[k] + n[k, z_next] + kappa * (z_next == k)) / (alpha + sum(n[k, ]) + kappa)
+    pzt[k] <- (alpha * beta[k] + n[z_prev, k] + kappa * (z_prev == k)) *
+      (alpha * beta[z_next] + n[k, z_next] + kappa * (z_next == k) + (z_prev == k) * (z_next == k)) *
+      (alpha + sum(n[k, ]) + kappa + (z_prev == k)) ^ (-1)
+  }
+  pzt[K + 1] <- (alpha ^ 2 * beta[K + 1] * beta[z_next]) / (alpha + kappa)
+
+  pzt
+}
+
+update_f <- function(yt, pzt, theta) {
+  K <- length(pzt) - 1
+  for (k in seq_len(K)) {
     f[k] <- f[k] * dmvt(yt, theta[k]$mu, theta[k]$sigma_sq, theta[k]$nu)
   }
-  f
 }
