60b82499247bd0b05d65835ea3e714d364173be3
Kris Sankaran <sankaran.kris@gmail.com>
2017-06-19 14:08:46 -0700

    state sampling weights

--- sticky_hdp_hmm/direct_sampler.R
+++ sticky_hdp_hmm/direct_sampler.R
@@ -7,6 +7,10 @@
 ## author: Kris Sankaran
 ## date: 06/19/2017
 
+## ---- libraries ----
+library("mvtnorm")
+
+## ---- utils ----
 #' @examples
 #' z <- c(1, 1, 2, 1, 1, 3, 3, 3, 1)
 #' transition_counts(z)
@@ -19,4 +23,25 @@ transition_counts <- function(z) {
     n[z[i], z[i + 1]] <- n[z[i], z[i + 1]] + 1
   }
   n
+}
+
+decrement_count <- function(n, z_prev, z_cur, z_next) {
+  n[z_prev, z_cur] <- n[z_prev, z_cur] - 1
+  n[z_cur, z_next] <- n[z_cur, z_next] - 1
+}
+
+increment_count <- function(n, z_prev, z_cur, z_next) {
+  n[z_prev, z_cur] <- n[z_prev, z_cur] + 1
+  n[z_cur, z_next] <- n[z_cur, z_next] + 1
+}
+
+f_vector <- function(yt, z_prev, z_next, n, alpha, beta, kappa, theta) {
+  K <- nrow(n)
+  f <- vector(length = K)
+
+  for (k in seq_len(K)) {
+    f[k] <- (alpha * beta[k] + n[k, z_next] + kappa * (z_next == k)) / (alpha + sum(n[k, ]) + kappa)
+    f[k] <- f[k] * dmvt(yt, theta[k]$mu, theta[k]$sigma_sq, theta[k]$nu)
+  }
+  f
 }
