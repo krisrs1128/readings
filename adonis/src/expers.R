3950e719d8f5c471a9663e0fff01a115322fde18
Kris Sankaran <sankaran.kris@gmail.com>
2017-06-20 10:40:34 -0700

    create unified data structure, across all configurations

--- adonis/src/expers.R
+++ adonis/src/expers.R
@@ -21,46 +21,46 @@ opts <- list(
   "sigma_x" = 1,
   "p_levels" = list(c(0.5, 0.5), c(0.5, 0.5), c(0.9, 0.1))
 )
+f_stats <- list()
 
 mod <- gaussian_null(opts, method = "euclidean", permutations = n_perm)
-plot_perms(mod, "gaussian_20_2.png")
+f_stats <- c(f_stats, list(perm_data(mod, opts)))
 
 opts <- modifyList(opts, list("n" = 100, "p" = 10))
 mod <- gaussian_null(opts, method = "euclidean", permutations = n_perm)
-plot_perms(mod, "gaussian_100_10.png")
-
+f_stats <- c(f_stats, list(perm_data(mod, opts)))
 
 opts <- modifyList(opts, list("n" = 20, "p" = 200))
 mod <- gaussian_null(opts, method = "euclidean", permutations = n_perm)
-plot_perms(mod, "gaussian_20_200.png")
+f_stats <- c(f_stats, list(perm_data(mod, opts)))
 
 opts <- modifyList(opts, list("n" = 20, "p" = 2000))
 mod <- gaussian_null(opts, method = "euclidean", permutations = n_perm)
-plot_perms(mod, "gaussian_20_2000.png")
+f_stats <- c(f_stats, list(perm_data(mod, opts)))
 
 ## ---- low-rank ----
 opts <- modifyList(opts, list("n" = 20, "p" = 2000, "K" = 10))
 mod <- low_rank_null(opts, method = "euclidean", permutations = n_perm)
-plot_perms(mod, "low_rank_20_2000.png")
+f_stats <- c(f_stats, list(perm_data(mod, opts)))
 
 ## ---- negative-binomial ----
 opts <- modifyList(opts, list("prob" = 0.01, "size" = 1))
 mod <- nb_null(opts, permutations = n_perm)
-plot_perms(mod, "nb_01_1.png")
+f_stats <- c(f_stats, list(perm_data(mod, opts)))
 
 opts <- modifyList(opts, list("prob" = 0.01, "size" = 1))
 mod <- nb_null(opts, permutations = n_perm / 100)
-plot_perms(mod, "nb_01_1_perm_100.png")
+f_stats <- c(f_stats, list(perm_data(mod, opts)))
 
 opts <- modifyList(opts, list("prob" = 0.01, "size" = 100))
 mod <- nb_null(opts, permutations = n_perm)
-plot_perms(mod, "nb_01_100.png")
+f_stats <- c(f_stats, list(perm_data(mod, opts)))
 
 opts <- modifyList(opts, list("prob" = 0.01, "size" = 100))
 mod <- nb_null(opts, permutations = n_perm, method = "euclidean")
-plot_perms(mod, "nb_euclidean_01_100.png")
+f_stats <- c(f_stats, list(perm_data(mod, opts)))
 
 ## ---- many-factors ----
 opts <- modifyList(opts, list("p_levels" = rep(list(c(0.5, 0.5)), 500)))
 mod <- gaussian_null(opts, method = "euclidean", permutations = n_perm)
-plot_perms(mod, "gaussian_large_p1_p2.png")
+f_stats <- c(f_stats, list(perm_data(mod, opts)))
