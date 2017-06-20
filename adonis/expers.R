32f3978a3731ba334491abf6ac4b58999969017c
Kris Sankaran <sankaran.kris@gmail.com>
2017-06-19 22:48:12 -0700

    ues plotting utility in experiments

--- adonis/expers.R
+++ adonis/expers.R
@@ -14,57 +14,30 @@ theme_set(ggscaffold::min_theme())
 
 ## ---- gaussian ----
 opts <- list(
-  "n" = 100,
-  "p" = 10,
+  "n" = 20,
+  "p" = 2,
   "mu" = 0,
   "sigma" = 1,
   "p_levels" = list(c(0.5, 0.5), c(0.5, 0.5), c(0.9, 0.1))
 )
-mod <- gaussian_null(opts, permutations = 10000)
-ggplot(as_data_frame(mod$f.perms)) +
-  geom_histogram(aes(x = V1), bins = 100)
 
-mod <- gaussian_null(
-  opts,
-  method = "euclidean",
-  permutations = 10000
-)
-ggplot(as_data_frame(mod$f.perms)) +
-  geom_histogram(aes(x = V1), bins = 250)
+mod <- gaussian_null(opts, method = "euclidean", permutations = 10000)
+plot_perms(mod)
 
-opts <- modifyList(
-  opts,
-  list("n" = 20, "p" = 200)
-)
-mod <- gaussian_null(
-  opts,
-  method = "euclidean",
-  permutations = 10000
-)
-ggplot(as_data_frame(mod$f.perms)) +
-  geom_histogram(aes(x = V1), bins = 250)
+opts <- modifyList(opts, list("n" = 100, "p" = 10))
+mod <- gaussian_null(opts, method = "euclidean", permutations = 10000)
+plot_perms(mod)
 
-opts <- modifyList(
-  opts,
-  list("n" = 20, "p" = 2000)
-)
-mod <- gaussian_null(
-  opts,
-  method = "euclidean",
-  permutations = 10000
-)
-ggplot(as_data_frame(mod$f.perms)) +
-  geom_histogram(aes(x = V1), bins = 250)
 
-opts <- modifyList(
-  opts,
-  list("n" = 20, "p" = 2)
-)
-mod <- gaussian_null(
-  opts,
-  method = "euclidean",
-  permutations = 10000
-)
-ggplot(as_data_frame(mod$f.perms)) +
-  geom_histogram(aes(x = V1), bins = 250)
+opts <- modifyList(opts, list("n" = 20, "p" = 200))
+mod <- gaussian_null(opts, method = "euclidean", permutations = 10000)
+plot_perms(mod)
 
+opts <- modifyList(opts, list("n" = 20, "p" = 2000))
+mod <- gaussian_null(opts, method = "euclidean", permutations = 10000)
+plot_perms(mod)
+
+## ---- low-rank ----
+opts <- modifyList(opts, list("n" = 20, "p" = 2000, "K" = 10))
+mod <- low_rank_null(opts, method = "euclidean", permutations = 10000)
+plot_perms(mod)
