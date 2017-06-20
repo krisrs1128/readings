df083b31d5e047165a2f7f3a3201bdeb070d7eb1
Kris Sankaran <sankaran.kris@gmail.com>
2017-06-20 11:13:36 -0700

    loop over a few different parameters

--- adonis/src/expers.R
+++ adonis/src/expers.R
@@ -27,51 +27,76 @@ i <- 1
 for (n in seq(40, 1000, length.out = 5)) {
   for (p in seq(2, 2000, length.out = 5)) {
     cat(sprintf("regime n: %f \t p: %f\n", n, p))
-    opts <- modifyList(opts, list("n" = 100, "p" = 10))
+    opts <- modifyList(opts, list("n" = n, "p" = p))
     mod <- gaussian_null(opts, permutations = n_perm)
     f_stats[[i]] <- perm_data(mod, opts)
     i <- i + 1
   }
 }
 
+ggplot(f_stats) +
+  geom_histogram(aes(x = f_perm)) +
+  facet_grid(n ~ p)
+
 ## ---- low-rank ----
-opts <- modifyList(opts, list("n" = 40, "p" = 2000, "K" = 10))
-mod <- low_rank_null(opts, permutations = n_perm)
-f_stats <- c(f_stats, list(perm_data(mod, opts)))
+f_list <- list()
+i <- 1
+opts$n <- 280
+opts$p <- 2000
+for (k in seq(2, 20, length.out = 5)) {
+  cat(sprintf("regime k: %f\n", k))
+  opts <- modifyList(opts, list("K" = k))
+  mod <- gaussian_null(opts, permutations = n_perm)
+  f_stats[[i]] <- perm_data(mod, opts)
+  i <- i + 1
+}
+
+ggplot(f_stats) +
+  geom_histogram(aes(x = f_perm)) +
+  facet_grid(~ k)
+
+## ---- num-perms ----
+f_list <- list()
+i <- 1
+for (nperm in 10 ^ (2:5)) {
+  cat(sprintf("regime n_perm: %f\n", n_perm))
+  opts <- modifyList(opts, list("K" = k))
+  mod <- gaussian_null(opts, permutations = n_perm)
+  f_stats[[i]] <- perm_data(mod, opts)
+  i <- i + 1
+}
+
+ggplot(f_stats) +
+  geom_histogram(aes(x = f_perm)) +
+  facet_grid(~ n_perm)
 
 ## ---- negative-binomial ----
-opts <- modifyList(
-  opts,
-  list("prob" = 0.01, "size" = 1, distribution = "nb", distance = "bray")
-)
-mod <- nb_null(opts, permutations = n_perm)
-f_stats <- c(f_stats, list(perm_data(mod, opts)))
+f_list <- list()
+i <- 1
+for (p in seq(0.01, 0.9, length.out = 5)) {
+  for (size in seq(1, 1000, length.out = 5)) {
+    opts <- modifyList(opts, list("prob" = 0.01, "size" = 100))
+    mod <- nb_null(opts, permutations = n_perm)
+    f_stats[[i]] <- perm_data(mod, opts)
+    i <- i + 1
+  }
+}
 
-opts <- modifyList(opts, list("prob" = 0.01, "size" = 1))
-mod <- nb_null(opts, permutations = n_perm / 100)
-f_stats <- c(f_stats, list(perm_data(mod, opts)))
-
-opts <- modifyList(opts, list("prob" = 0.01, "size" = 100))
-mod <- nb_null(opts, permutations = n_perm)
-f_stats <- c(f_stats, list(perm_data(mod, opts)))
-
-opts <- modifyList(opts, list("prob" = 0.01, "size" = 100))
-mod <- nb_null(opts, permutations = n_perm)
-f_stats <- c(f_stats, list(perm_data(mod, opts)))
+ggplot(f_stats) +
+  geom_histogram(aes(x = f_perm)) +
+  facet_grid(~ p)
 
 ## ---- many-factors ----
-opts <- modifyList(opts, list("p_levels" = rep(list(c(0.5, 0.5)), 500)))
-mod <- gaussian_null(opts, permutations = n_perm)
-f_stats <- c(f_stats, list(perm_data(mod, opts)))
+i <- 1
+f_list <- list()
+for (p2 in seq(2, 500, length.out = 4)) {
+  cat(sprintf("regime p2: %f\n", p2))
+  opts <- modifyList(opts, list("p_levels" = rep(list(c(0.5, 0.5)), p2)))
+  mod <- gaussian_null(opts, permutations = n_perm)
+  f_stats[[i]] <- perm_data(mod, opts)
+  i <- i + 1
+}
 
-## ---- plot-all ----
-f_stats <- do.call(bind_rows, f_stats)
-
-ggplot(
-  f_stats %>%
-  filter(distribution == "gaussian", is.na(K))
-) +
-  geom_histogram(
-    aes(x = f_perm)
-  ) +
-  facet_wrap(n~p, scales = "free")
+ggplot(f_stats) +
+  geom_histogram(aes(x = f_perm)) +
+  facet_grid(~ p2)
