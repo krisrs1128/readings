24838380003625ad9dd659e637fff2c15d0e9ca2
Kris Sankaran <sankaran.kris@gmail.com>
2017-06-20 10:21:56 -0700

    consider case with many null factors

--- adonis/src/expers.R
+++ adonis/src/expers.R
@@ -41,7 +41,7 @@ plot_perms(mod, "gaussian_20_2000.png")
 ## ---- low-rank ----
 opts <- modifyList(opts, list("n" = 20, "p" = 2000, "K" = 10))
 mod <- low_rank_null(opts, method = "euclidean", permutations = n_perm)
-plot_perms(mod, "low_rank_20_20000.png")
+plot_perms(mod, "low_rank_20_2000.png")
 
 ## ---- negative-binomial ----
 opts <- modifyList(opts, list("prob" = 0.01, "size" = 1))
@@ -59,3 +59,8 @@ plot_perms(mod, "nb_01_100.png")
 opts <- modifyList(opts, list("prob" = 0.01, "size" = 100))
 mod <- nb_null(opts, permutations = n_perm, method = "euclidean")
 plot_perms(mod, "nb_euclidean_01_100.png")
+
+## ---- many-factors ----
+opts <- modifyList(opts, list("p_levels" = rep(list(c(0.5, 0.5)), 500)))
+mod <- gaussian_null(opts, method = "euclidean", permutations = n_perm)
+plot_perms(mod, "gaussian_large_p1_p2.png")
