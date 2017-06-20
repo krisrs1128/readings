64cafdade62fbae4a33c8876e8db4d44b1f2338d
Kris Sankaran <sankaran.kris@gmail.com>
2017-06-20 10:59:54 -0700

    loop over n / p ratios

--- adonis/src/expers.R
+++ adonis/src/expers.R
@@ -13,38 +13,37 @@ source("utils.R")
 theme_set(ggscaffold::min_theme())
 
 ## ---- gaussian ----
-n_perm <- 100000
+n_perm <- 1000
 opts <- list(
-  "n" = 20,
-  "p" = 2,
   "mu_x" = 0,
   "sigma_x" = 1,
-  "p_levels" = list(c(0.5, 0.5), c(0.5, 0.5), c(0.9, 0.1))
+  "p_levels" = list(c(0.5, 0.5), c(0.5, 0.5), c(0.8, 0.2)),
+  "distribution" = "gaussian",
+  "distance" = "euclidean"
 )
+
 f_stats <- list()
-
-mod <- gaussian_null(opts, method = "euclidean", permutations = n_perm)
-f_stats <- c(f_stats, list(perm_data(mod, opts)))
-
-opts <- modifyList(opts, list("n" = 100, "p" = 10))
-mod <- gaussian_null(opts, method = "euclidean", permutations = n_perm)
-f_stats <- c(f_stats, list(perm_data(mod, opts)))
-
-opts <- modifyList(opts, list("n" = 20, "p" = 200))
-mod <- gaussian_null(opts, method = "euclidean", permutations = n_perm)
-f_stats <- c(f_stats, list(perm_data(mod, opts)))
-
-opts <- modifyList(opts, list("n" = 20, "p" = 2000))
-mod <- gaussian_null(opts, method = "euclidean", permutations = n_perm)
-f_stats <- c(f_stats, list(perm_data(mod, opts)))
+i <- 1
+for (n in seq(40, 1000, length.out = 5)) {
+  for (p in seq(2, 2000, length.out = 5)) {
+    cat(sprintf("regime n: %f \t p: %f\n", n, p))
+    opts <- modifyList(opts, list("n" = 100, "p" = 10))
+    mod <- gaussian_null(opts, permutations = n_perm)
+    f_stats[[i]] <- perm_data(mod, opts)
+    i <- i + 1
+  }
+}
 
 ## ---- low-rank ----
-opts <- modifyList(opts, list("n" = 20, "p" = 2000, "K" = 10))
-mod <- low_rank_null(opts, method = "euclidean", permutations = n_perm)
+opts <- modifyList(opts, list("n" = 40, "p" = 2000, "K" = 10))
+mod <- low_rank_null(opts, permutations = n_perm)
 f_stats <- c(f_stats, list(perm_data(mod, opts)))
 
 ## ---- negative-binomial ----
-opts <- modifyList(opts, list("prob" = 0.01, "size" = 1))
+opts <- modifyList(
+  opts,
+  list("prob" = 0.01, "size" = 1, distribution = "nb", distance = "bray")
+)
 mod <- nb_null(opts, permutations = n_perm)
 f_stats <- c(f_stats, list(perm_data(mod, opts)))
 
@@ -57,10 +56,22 @@ mod <- nb_null(opts, permutations = n_perm)
 f_stats <- c(f_stats, list(perm_data(mod, opts)))
 
 opts <- modifyList(opts, list("prob" = 0.01, "size" = 100))
-mod <- nb_null(opts, permutations = n_perm, method = "euclidean")
+mod <- nb_null(opts, permutations = n_perm)
 f_stats <- c(f_stats, list(perm_data(mod, opts)))
 
 ## ---- many-factors ----
 opts <- modifyList(opts, list("p_levels" = rep(list(c(0.5, 0.5)), 500)))
-mod <- gaussian_null(opts, method = "euclidean", permutations = n_perm)
+mod <- gaussian_null(opts, permutations = n_perm)
 f_stats <- c(f_stats, list(perm_data(mod, opts)))
+
+## ---- plot-all ----
+f_stats <- do.call(bind_rows, f_stats)
+
+ggplot(
+  f_stats %>%
+  filter(distribution == "gaussian", is.na(K))
+) +
+  geom_histogram(
+    aes(x = f_perm)
+  ) +
+  facet_wrap(n~p, scales = "free")
