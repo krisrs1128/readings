1032c79f69572181a61f866627db6e6deb0636e6
Kris Sankaran <sankaran.kris@gmail.com>
2017-06-29 23:14:49 -0700

    em for parameter estimation

--- lgm/lds.R
+++ lgm/lds.R
@@ -75,13 +75,14 @@ lds_inference <- function(y, A, C, Q, R, x01, v01) {
   p <- nrow(C)
   k <- nrow(A)
 
+  #' initialize for the forwards pass
   x_predict <- x01
   x_filter <- matrix(nrow = time_len, ncol = k)
   v_predict <- array(dim = c(k, k, time_len))
   v_predict[,, 1] <- v01
   v_filter <- array(dim = c(k, k, time_len))
 
-  #' forward pass
+  #' make the forward pass
   for (i in seq_len(time_len)) {
     if (i > 1) {
       x_predict <- A %*% x_filter[i - 1, ]
@@ -93,13 +94,15 @@ lds_inference <- function(y, A, C, Q, R, x01, v01) {
     v_filter[,, i] <- v_predict[,, i] - K %*% C %*% v_predict[,, i]
   }
 
-  #' backwards pass
+  #' initialize for backwards pass
   x_smooth <- matrix(nrow = time_len, ncol = k)
   x_smooth[time_len, ] <- x_filter[time_len, ]
   v_smooth <- array(dim = c(k, k, time_len))
+  v_smooth[,, time_len] <- v_filter[,, time_len]
   v_pair <- array(dim = c(k, k, time_len - 1))
   v_pair[,, time_len - 1] <- (diag(k) - K %*% C) %*% A %*% v_filter[,, time_len - 1]
 
+  #' make the backwards pass
   for (i in seq(time_len, 2)) {
     J_prev <- v_filter[,, i - 1] %*% t(A) %*% solve(v_predict[,, i])
 
@@ -109,7 +112,7 @@ lds_inference <- function(y, A, C, Q, R, x01, v01) {
       J_prev %*% (v_smooth[,, i] - v_predict[,, i]) %*% t(J_prev)
 
     if (i < time_len) {
-      v_pair[,, i - 1] <- v_filter[,, i] +
+      v_pair[,, i - 1] <- v_filter[,, i] %*% t(J_prev) +
         J_next %*% (v_pair[,, i] - A %*% v_filter[,, i]) %*% t(J_prev)
     }
     J_next <- J_prev
@@ -124,13 +127,22 @@ lds_inference <- function(y, A, C, Q, R, x01, v01) {
   )
 }
 
+#' @examples
+#' A <- diag(0.99, nrow = 1)
+#' C <- diag(1, nrow = 1)
+#' Q <- diag(0.1, nrow = 1)
+#' R <- diag(1, nrow = 1)
+#' lds <- simulate(A, C, 0, Q, R, time_len = 100)
+#' res <- lds_learn(lds$y, k = 1)
+#' plot(lds$y)
+#' points(res$x_smooth, col = "blue")
 lds_learn <- function(y, k, eps = 0.001) {
   time_len <- nrow(y)
   p <- ncol(y)
 
   #' initialize parameters
   A <- diag(0.5, nrow = k)
-  C <- matrix(runif(p * k), p, k)
+  C <- matrix(0.5, p, k)
   Q <- diag(1, k)
   R <- diag(1, p)
   x01 <- rep(0, k)
@@ -139,7 +151,7 @@ lds_learn <- function(y, k, eps = 0.001) {
   alpha <- t(y) %*% y
 
   #' not calculating loglik right now...
-  for (iter in seq_len(100)) {
+  for (iter in seq_len(10)) {
 
     #' E-step
     inf <- lds_inference(y, A, C, Q, R, x01, v01)
@@ -152,11 +164,11 @@ lds_learn <- function(y, k, eps = 0.001) {
     beta <- 0
 
     for (i in seq_len(time_len)) {
-      delta <- delta + t(y) %*% x_smooth[i, ]
-      gamma <- gamma + x_smooth[i, ] %*% x_smooth[i, ] + v_smooth[,, i]
+      delta <- delta + y[i, ] %*% t(x_smooth[i, ])
+      gamma <- gamma + x_smooth[i, ] %*% t(x_smooth[i, ]) + v_smooth[,, i]
 
       if (i > 1) {
-        beta <- beta + x_smooth[i, ] %*% t(x_smooth[i - 1, ]) + v_pair[,, i]
+        beta <- beta + x_smooth[i, ] %*% t(x_smooth[i - 1, ]) + v_pair[,, i - 1]
       }
     }
 
@@ -179,8 +191,10 @@ lds_learn <- function(y, k, eps = 0.001) {
     "C" = C,
     "Q" = Q,
     "R" = R,
-    "v01" = x01,
-    "v01" = V01
+    "x01" = x01,
+    "v01" = v01,
+    "x_smooth" = x_smooth,
+    "v_smooth" = v_smooth
   )
 }
 
