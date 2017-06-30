b1e5944e23eaeb7b6364c38232d11642071df7bb
Kris Sankaran <sankaran.kris@gmail.com>
2017-06-29 21:10:19 -0700

    create lds file

---
+++ lgm/lds.R
@@ -0,0 +1,17 @@
+#! /usr/bin/env Rscript
+
+#' File description -------------------------------------------------------------
+#' An attempt to implement EM for the linear dynamic system as described in
+#' section A.3.1 of
+#'
+#' Roweis, Sam, and Zoubin Ghahramani. "A unifying review of linear Gaussian
+#' models." Neural computation 11.2 (1999): 305-345.
+#'
+#' author: sankaran.kris@gmail.com
+#' date: 6/29/2017
+
+lds_inference <- function(Y, A, C, Q, R, x0_1, V0_1) {
+}
+
+lds_learn <- function(Y, k, c) {
+}
