01e51b4aa46727c2a19cc6895f0b5d11edf12a21
Kris Sankaran <sankaran.kris@gmail.com>
2017-07-18 11:24:33 -0700

    Add an example with the antibiotics data set

--- scan_sampler/exper.R
+++ scan_sampler/exper.R
@@ -46,3 +46,41 @@ p <- ggplot() +
   )
 
 p + ylim(-10, 5)
+
+## antibiotics data
+library("treelapse")
+library("phyloseq")
+data(abt)
+abt <- abt %>%
+  filter_taxa(function(x) var(x) > 4, prune = TRUE) %>%
+  subset_samples(ind == "F") %>%
+  transform_sample_counts(asinh)
+y <- get_taxa(abt)[3, ]
+
+y_samples <- sampler(y, A, C, Q, R, n_iter = 100)
+
+y_samples <- abind(y, y_samples, along = 3)
+my <- melt(
+  y_samples,
+  varnames = c("i", "p", "iter"),
+  value.name = "y"
+)
+
+p <- ggplot() +
+  geom_point(
+    data = my %>% filter(y < 0),
+    aes(x = i, y = y),
+    size = .5, alpha = 0.05
+  ) +
+  geom_point(
+    data = my %>% filter(iter == 2),
+    aes(x = i, y = y),
+    col = "red", size = 1, alpha = 1
+  ) +
+  geom_point(
+    data = my %>% filter(iter == 1),
+    aes(x = i, y = y),
+    col = "red", size = 1, alpha = 0.5
+  )
+
+p + ylim(-15, 5)
