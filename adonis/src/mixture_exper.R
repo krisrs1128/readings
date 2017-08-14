427a711c7a02366d62cd87f542a4fd819ac05b38
Kris Sankaran <sankaran.kris@gmail.com>
2017-08-14 13:23:15 -0700

    simulate variation in y according to lda

--- adonis/src/mixture_exper.R
+++ adonis/src/mixture_exper.R
@@ -91,32 +91,33 @@ rmix_gauss <- function(n, mu_mat, Sigma = NULL, probs = NULL) {
 #'
 #' @examples
 #' rlda(100, 10)
-rlda <- function(n, V, N = 500, K = 4, alpha0 = 1) {
+rlda <- function(n, theta, V, N = 500, K = 4, alpha0 = 1) {
   beta <- rdirichlet(K, alpha0 * rep(1, V))
-  theta <- rdirichlet(n, alpha0 * rep(1, K))
   x <- matrix(nrow = n, ncol = V)
   for (i in seq_len(n)) {
     x[i, ] <- rmultinom(1, N, prob = t(beta) %*% theta[i, ])
   }
 
-  x
+  list("x" = x, "theta" = theta)
+}
+
+sample_probs <- function(probs) {
+  y <- vector(length = nrow(probs))
+  for (i in seq_len(n)) {
+    y[i] <- sample(0:1, 1, replace = TRUE, prob = c(1 - prob[i, ], prob[i, ]))
+  }
+
+  y
 }
 
 ###############################################################################
 ## simulation experiment
 ###############################################################################
 
-## generate matrix y according to logit
-probs <- t(logit(rmvnorm(n_sim, sigma = K)))
-y <- matrix(0, n, n_sim)
-for (i in seq_len(n)) {
-  for (j in seq_len(n_sim)) {
-    y[i, j] <- sample(
-      0:1, 1,
-      prob = c(1 - probs[i, j], probs[i, j])
-    )
-  }
-}
+## generate matrix y reflecting mixture structure
+theta <- rdirichlet(n, rep(1, 4))
+prob <- logit(theta %*% rnorm(4, 0, 2.5))
+y_lda <- sample_probs(prob)
 
 ## setup for factor analysis and mixture of gaussians
 k <- 1
@@ -142,10 +143,10 @@ for (i in seq_len(n_sim)) {
   models$mix_gauss$lm[[i]] <- lm(x[, 1] ~ y[, i] + u)
 
   ## latent dirichlet allocation
-  x <- rlda(n, p2)
-  models$lda$adonis[[i]] <- adonis(x ~ y[, i] + u, method = "euclidean")
-  models$lda$logistic[[i]] <- glm(y[, i] ~ x + u, family = binomial())
-  models$lda$lm[[i]] <- lm(x[, 1] ~ y[, i] + u)
+  x <- rlda(n, theta, p2)
+  models$lda$adonis[[i]] <- adonis(x ~ y_lda[, i], method = "euclidean")
+  models$lda$logistic[[i]] <- glm(y_lda[, i] ~ x, family = binomial())
+  models$lda$lm[[i]] <- lm(x[, 1] ~ y_lda[, i])
 }
 
 pvals <- list(
