7b5a0e659d5d570cb9cd64607d8846aa5c87d2e0
Kris Sankaran <sankaran.kris@gmail.com>
2017-08-14 18:53:46 -0700

    gam fit gives valid p-values

--- adonis/src/gp_exper.R
+++ adonis/src/gp_exper.R
@@ -30,7 +30,7 @@ theme_set(
 ###############################################################################
 
 ## variables used throughout experiment
-n_sim <- 1000
+n_sim <- 400
 p1 <- 15
 p2 <- 2
 n <- 100
@@ -62,12 +62,12 @@ for (i in seq_len(n_sim)) {
   f <- t(rmvnorm(p1, sigma = K))
   x <- matrix(rpois(n * p1, lambda = exp(f)), n, p1)
   y <- sample_probs(probs[, i])
-  models$poisson$adonis[[i]] <- adonis(x ~ y + u, method = "bray")
+  models$poisson$adonis[[i]] <- adonis(x ~ y + u, method = "bray", perm = 10)
   models$poisson$logistic[[i]] <- glm(y ~ x + u, family = binomial())
   models$poisson$lm[[i]] <- glm(x[, 1] ~ y + u, family = "poisson")
   models$poisson$gls[[i]] <- gls(x[, 1] ~ y + u)
   models$poisson$mds[[i]] <- mds_lm(x, cbind(y, u))
-  models$poisson$gam[[i]] <- gam(x[, 1] ~ y + te(u))
+  models$poisson$gam[[i]] <- gam(x[, 1] ~ y + ti(u[, 1]) + ti(u[, 2]) + ti(u[, 1], u[, 2]))
 }
 
 ## Gaussian setup
@@ -75,12 +75,12 @@ for (i in seq_len(n_sim)) {
   print_iter(i)
   y <- sample_probs(probs[, i])
   x <- t(rmvnorm(p1, sigma = K))
-  models$gaussian$adonis[[i]] <- adonis(x ~ y + u, method = "euclidean")
+  models$gaussian$adonis[[i]] <- adonis(x ~ y + u, method = "euclidean", perm = 10)
   models$gaussian$logistic[[i]] <- glm(y ~ x + u, family = binomial())
   models$gaussian$lm[[i]] <- lm(x[, 1] ~ y + u)
   models$gaussian$gls[[i]] <- gls(x[, 1] ~ y + u)
   models$gaussian$mds[[i]] <- mds_lm(x, cbind(y, u))
-  models$gaussian$gam[[i]] <- gam(x[, 1] ~ y + te(u, bs = "tp"))
+  models$gaussian$gam[[i]] <- gam(x[, 1] ~ y + ti(u[, 1]) + ti(u[, 2]) + ti(u[, 1], u[, 2]))
 }
 
 ###############################################################################
