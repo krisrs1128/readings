d61bf2e02f18ce0f8ac114773133fba82c7e210e
Kris Sankaran <sankaran.kris@gmail.com>
2017-08-27 23:19:21 -0700

    checking derivative for v1

--- igp_mix/hmc_exper.jl
+++ igp_mix/hmc_exper.jl
@@ -185,3 +185,28 @@ check_derivative(reference_f, test_f, 10.0)
 check_derivative(reference_f, test_f, 2.0)
 check_derivative(reference_f, test_f, 1.0)
 check_derivative(reference_f, test_f, 0.0)
+
+function k_deriv_v1(n::Int64)
+  function(log_v1::Float64)
+    k_theta_deriv = exp(log_v1) * eye(n)
+    k_theta_deriv[10, 2]
+  end
+end
+
+function k_theta_v1(
+  y::Vector,
+  x::Matrix,
+  log_l2::Float64,
+  log_v0::Float64)
+  function(log_v1::Float64)
+    k_theta = kernel(x, x, sqrt(exp(log_l2)), exp(log_v0), exp(log_v1))
+    k_theta[10, 2]
+  end
+end
+
+reference_f = k_theta_v1(y, x, theta[3], theta[1])
+test_f = k_deriv_v1(length(y))
+check_derivative(reference_f, test_f, 10.0)
+check_derivative(reference_f, test_f, 2.0)
+check_derivative(reference_f, test_f, 1.0)
+check_derivative(reference_f, test_f, 0.0)
