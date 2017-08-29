812de87c8906fa213026babc5c67e5327d688845
Kris Sankaran <sankaran.kris@gmail.com>
2017-08-29 13:45:23 -0700

    Separate script and utils files

--- igp_mix/hmc_exper.jl
+++ igp_mix/hmc_exper.jl
@@ -12,192 +12,33 @@
 # date: 08/24/2017
 
 using Distributions
-using Gadfly
-using Mamba
-
-###############################################################################
-#                        Define types for GP inference                        #
-###############################################################################
-type KernelParam
-  l::Float64
-  v0::Float64
-  v1::Float64
-end
-
-
-type GPHyper
-  log_l2
-  log_v0
-  log_v1
-end
-
-
-type GP
-  theta::KernelParam
-  a::GPHyper
-end
-
-###############################################################################
-#                      Define kernel and its derivatives                      #
-###############################################################################
-
-"""Covariance Kernel with Noise
-
-# Arguments
-x::Matrix: Each row gives a value for rows x_{i} in the matrix K(x_{i}, y_{j})
-y::Matrix: Each row gives a value for columns y_{j} in the matrix K(x_{i}, y_{j})
-theta::KernelParam: Parameters for the kernel (bandwidth l, amplitude v0, and
-  noise v1)
-
-# Examples
-```julia-repl
-theta = KernelParam(1, .2, 1)
-kernel(rand(10, 2), rand(10, 2), theta)
-```
-"""
-function kernel(x::Matrix, y::Matrix, theta::KernelParam)
-  n1 = size(x, 1)
-  n2 = size(y, 1)
-  K = zeros(n1, n2)
-
-  for i = 1:n1
-    for j = 1:n2
-      K[i, j] = theta.v0 * exp(- 1 / (2 * theta.l ^ 2) * norm(x[i, :] - y[j, :]) ^ 2)
-      if i == j
-        K[i, j] = theta.v1 + K[i, j]
-      end
-    end
-  end
-
-  K
-end
-
-"""Gradient of Marginal Likelihood
-
-Computes the gradient of the marginal likelihood, given dK / dtheta
-
-# Arguments
-k_theta::Matrix: The kernel matrix evaluated at some coordinates x
-y::Vector: The response at the coordinates x
-k_deriv: The matrix dK / dtheta
-"""
-function marginal_grad(k_theta::Matrix, y::Vector, k_deriv::Matrix)
-  alpha = inv(k_theta) * y
-  return trace((alpha * alpha' - inv(k_theta)) * k_deriv)
-end
-
-
-"""Derivative of Kernel w.r.t log-amplitude log_v0
-
-# Arguments
-x::Matrix: Coordinates at which the kernel matrix was evaluated
-theta::KernelParam: Parameters for the kernel (bandwidth l, amplitude v0, and
-  noise v1)
-"""
-function k_deriv_logv0(x::Matrix, theta::KernelParam)
-  theta.v1 = 0
-  kernel(x, x, theta)
-end
-
-
-"""Derivative of Kernel w.r.t. log-bandwidth log_l2
-
-# Arguments
-x::Matrix: Coordinates at which the kernel matrix was evaluated
-theta::KernelParam: Parameters for the kernel (bandwidth l, amplitude v0, and
-  noise v1)
-"""
-function k_deriv_logl2(x::Matrix, theta::KernelParam)
-  n = size(x, 1)
-  k_deriv = zeros(n, n)
-  for i = 1:n
-    for j = 1:n
-      norm_sq = norm(x[i, :] - x[j, :]) ^ 2
-      k_deriv[i, j] = theta.v0 * exp(- 1 / (2 * theta.l ^ 2) * norm_sq) *
-        norm_sq / (2 * theta.l ^ 2)
-    end
-  end
-
-  k_deriv
-end
-
-
-"""Gradient Functions at given Coordinates
-
-# Arguments
-y::Vector: The response at the coordinates x
-x::Matrix: Coordinates at which the kernel matrix was evaluated
-a::GPHyper: Priors for parameters in kernel function
-"""
-function gradient_generator(y::Vector, x::Matrix, a::GPHyper)
-
-  function log_posterior_grad(log_theta::Vector)
-    log_l2 = log_theta[1]
-    log_v0 = log_theta[2]
-    log_v1 = log_theta[3]
-    theta = KernelParam(sqrt(exp(log_l2)), exp(log_v0), exp(log_v1))
-
-    k_theta = kernel(x, x, theta)
-    n = length(y)
-
-    ## compute (unnormalized) posterior
-    unnorm_posterior = logpdf(MvNormal(zeros(n), k_theta), y) +
-      logpdf(a.log_l2, log_v0) +
-      logpdf(a.log_v0, log_v1) +
-      logpdf(a.log_v1, log_l2)
-
-    ## compute gradient
-    grad_logl2 = marginal_grad(k_theta, y, k_deriv_logl2(x, theta)) +
-      gradlogpdf(a.log_l2, log_l2)
-    grad_logv0 = marginal_grad(k_theta, y, k_deriv_logv0(x, theta)) +
-      gradlogpdf(a.log_v0, log_v0)
-    grad_logv1 = marginal_grad(k_theta, y, exp(log_v1) * eye(n)) +
-      gradlogpdf(a.log_v1, log_v1)
-
-    unnorm_posterior, [grad_logl2, grad_logv0, grad_logv1]
-  end
-end
-
-"""Simulate toy data
-
-# Examples
-```julia-repl
-julia> n = 100
-julia> theta = KernelParam()
-julia> simulate(n, theta)
-```
-"""
-function simulate(n::Int64, theta::KernelParam)
-  x = rand(n, 1)
-  K = kernel(x, x, theta)
-  y = rand(MultivariateNormal(zeros(n), K))
-
-  return x, y
-end
+include("igp_mix.jl")
 
 theta = KernelParam(sqrt(0.05), 10.0, 0.5)
-x, y = simulate(50, theta)
-plot(x = x[:, 1], y = y)
+x, y = simulate(70, theta)
 
-n_samples = 1000
-samples = zeros(n_samples, 3)
 a = GPHyper(
   Distributions.TDist(1),
   Distributions.TDist(1),
   Distributions.TDist(1)
 )
-logf_grad = gradient_generator(y, x, a)
-L = 5
-epsilon = 0.005
-sampler = HMCVariate(samples[1, :], epsilon, L, logf_grad)
-for i = 1:n_samples
-  sample!(sampler)
-  samples[i, :] = exp.(sampler)
-  print(i)
-  println(samples[i, :])
+samples = GPSampler(x, y, a, 20, 5, 0.001, [-1, 2, -1])
+
+x_new = collect(0:0.01:1)[:, :]
+post_data = zeros(0, 4)
+for i in 1:size(samples, 1)
+  if i % 50 != 0
+    next
+  end
+
+  theta_fit = param_from_theta(samples[i, :])
+  post = gp_posterior(x_new, GPModel(theta_fit, x, y))
+  post_data = [
+    post_data;
+    i * ones(size(x_new, 1)) x_new mean(post) var(post)
+  ]
 end
 
-plot(x = samples[:, 1], y = samples[:, 2])
-plot(x = 1:n_samples, y = samples[:, 1])
-plot(x = 1:n_samples, y = samples[:, 2])
-plot(x = 1:n_samples, y = samples[:, 3])
+writedlm("train_data.csv", [x y])
+writedlm("samples.csv", samples)
+writedlm("post_data.csv", post_data)
