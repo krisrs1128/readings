3c1517d4fe85e913511263f5ecc94d17ac069cdf
Kris Sankaran <sankaran.kris@gmail.com>
2017-08-29 14:08:57 -0700

    logistic prior seems a little better

--- igp_mix/hmc_exper.jl
+++ igp_mix/hmc_exper.jl
@@ -18,19 +18,20 @@ theta = KernelParam(sqrt(0.05), 10.0, 0.5)
 x, y = simulate(70, theta)
 
 a = GPHyper(
-  Distributions.TDist(1),
-  Distributions.TDist(1),
-  Distributions.TDist(1)
+  Distributions.Logistic(-1, 4),
+  Distributions.Logistic(-1, 4),
+  Distributions.Logistic(-1, 4)
 )
-samples = GPSampler(x, y, a, 20, 5, 0.001, [-1, 2, -1])
+samples = GPSampler(x, y, a, 5000, 5, 0.005, zeros(3))
 
 x_new = collect(0:0.01:1)[:, :]
 post_data = zeros(0, 4)
 for i in 1:size(samples, 1)
-  if i % 50 != 0
-    next
+  if (i - 1) % 50 != 0
+    continue
   end
 
+  println(i)
   theta_fit = param_from_theta(samples[i, :])
   post = gp_posterior(x_new, GPModel(theta_fit, x, y))
   post_data = [
@@ -39,6 +40,6 @@ for i in 1:size(samples, 1)
   ]
 end
 
-writedlm("train_data.csv", [x y])
-writedlm("samples.csv", samples)
-writedlm("post_data.csv", post_data)
+writedlm("data/train_data.tsv", [x y])
+writedlm("data/samples.tsv", samples)
+writedlm("data/post_data.tsv", post_data)
