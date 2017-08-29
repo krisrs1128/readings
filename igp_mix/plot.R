9fde49898e208211c94f45cd76122b041ccbadbd
Kris Sankaran <sankaran.kris@gmail.com>
2017-08-29 14:09:08 -0700

    plot variances as well

--- igp_mix/plot.R
+++ igp_mix/plot.R
@@ -9,30 +9,49 @@
 
 library("tidyverse")
 
+scale_colour_discrete <- function(...)
+  scale_colour_brewer(..., palette="Set2")
+scale_fill_discrete <- function(...)
+  scale_fill_brewer(..., palette="Set2")
+
+theme_set(theme_bw())
+min_theme <- theme_update(
+  panel.border = element_blank(),
+  panel.grid = element_blank(),
+  axis.ticks = element_blank(),
+  legend.title = element_text(size = 8),
+  legend.text = element_text(size = 6),
+  axis.text = element_text(size = 6),
+  axis.title = element_text(size = 8),
+  strip.background = element_blank(),
+  strip.text = element_text(size = 8),
+  legend.key = element_blank()
+)
+
 ## Load data
-train_data <- read_tsv("train_data.csv", col_names = F) %>%
-  rename(x = X, y = X2)
-post <- read_tsv("post_data.csv", col_names = F) %>%
+train_data <- read_tsv("data/train_data.tsv", col_names = F) %>%
+  rename(x = X1, y = X2)
+post <- read_tsv("data/post_data.tsv", col_names = F) %>%
   rename(iter = X1, x_new = X2, mu = X3, var = X4)
 
 ## Plot
 ggplot() +
   geom_line(
     data = post,
-    aes(x = x_new, y = mu, alpha = iter)
+    aes(x = x_new, y = mu, group = iter),
+    alpha = 0.05
   ) +
   geom_line(
     data = post,
-    aes(x = x_new, y = mu + 1.96 * var, alpha = iter),
-    col = "red"
+    aes(x = x_new, y = mu + 1.96 * sqrt(var), group = iter),
+    alpha = 0.05, col = "red"
   ) +
   geom_line(
     data = post,
-    aes(x = x_new, y = mu - 1.96 * var, alpha = iter),
-    col = "red"
+    aes(x = x_new, y = mu - 1.96 * sqrt(var), group = iter),
+    alpha = 0.05, col = "red"
   ) +
   geom_point(
     data = train_data,
     aes(x = x, y = y)
-  ) +
-  scale_alpha(range = c(0, 0.03))
+  )
