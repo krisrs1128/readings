f52f27f1cd3b035e13f8ec74a9f8a2982304693e
Kris Sankaran <sankaran.kris@gmail.com>
2017-08-29 11:27:12 -0700

    more succinct derivative testing

--- igp_mix/test.jl
+++ igp_mix/test.jl
@@ -1,77 +1,61 @@
 
+function k_coords(x::Matrix,
+                  theta::KernelParam,
+                  i::Int64 = 1,
+                  j::Int64 = 2)
+  f_list = Dict{String, Function}()
 
-function k_deriv_lij(x::Matrix, log_v0::Float64)
-  function(log_l2::Float64)
-    k_theta_deriv = k_deriv_logl2(x, log_v0, log_l2)
-    k_theta_deriv[1, 2]
+  f_list["l"] = function(log_l2::Float64)
+    theta.l = sqrt(exp(log_l2))
+    kernel(x, x, theta)[i, j]
   end
+
+  f_list["v0"] = function(log_v0::Float64)
+    theta.v0 = exp(log_v0)
+    kernel(x, x, theta)[i, j]
+  end
+
+  f_list["v1"] = function(log_v1::Float64)
+    theta.v1 = exp(log_v1)
+    kernel(x, x, theta)[i, j]
+  end
+
+  f_list
 end
 
-function k_theta_lij(
-  y::Vector,
-  x::Matrix,
-  log_v0::Float64,
-  log_v1::Float64)
-  function(log_l2::Float)
-    k_theta = kernel(x, x, sqrt(exp(log_l2)), exp(log_v0), exp(log_v1))
-    k_theta[1, 2]
+
+function k_deriv_coords(x::Matrix,
+                        theta::KernelParam,
+                        i::Int64 = 1,
+                        j::Int64 = 2)
+  f_list = Dict{String, Function}()
+
+  f_list["log_l2"] = function(log_l2::Float64)
+    theta.l = sqrt(exp(log_l2))
+    k_theta_deriv = k_deriv_logl2(x, theta)
+    k_theta_deriv[i, j]
   end
+
+  f_list["log_v0"] = function(log_v0::Float64)
+    theta.v0 = exp(log_v0)
+    k_theta_deriv = k_deriv_logv0(x, theta)
+    k_theta_deriv[i, j]
+  end
+
+  f_list["log_v1"] = function(log_v1::Float64)
+    k_theta_deriv = exp(log_v1) * eye(size(x, 1))
+    k_theta_deriv[i, j]
+  end
+
+  f_list
 end
 
-reference_f = k_theta_lij(y, x, theta[1], theta[2])
-test_f = k_deriv_lij(x, theta[1])
-check_derivative(reference_f, test_f, 10.0)
-check_derivative(reference_f, test_f, 2.0)
-check_derivative(reference_f, test_f, 1.0)
-check_derivative(reference_f, test_f, 0.0)
+coord_f = k_coords(x, theta)
+deriv_f = k_deriv_coords(x, theta)
 
-
-function k_deriv_v0(x::Matrix, log_l2::Float64)
-  function(log_v0::Float64)
-    k_theta_deriv = k_deriv_logv0(x, log_v0, log_l2)
-    k_theta_deriv[10, 2]
-  end
+for u in rand(Uniform(-10, 10), 10)
+  println(check_derivative(coord_f["l"], deriv_f["log_l2"], u))
+  println(check_derivative(coord_f["v0"], deriv_f["log_v0"], u))
+  println(check_derivative(coord_f["v1"], deriv_f["log_v1"], u))
 end
 
-function k_theta_v0(
-  y::Vector,
-  x::Matrix,
-  log_l2::Float64,
-  log_v1::Float64)
-  function(log_v0::Float64)
-    k_theta = kernel(x, x, sqrt(exp(log_l2)), exp(log_v0), exp(log_v1))
-    k_theta[10, 2]
-  end
-end
-
-reference_f = k_theta_v0(y, x, theta[3], theta[2])
-test_f = k_deriv_v0(x, theta[3])
-check_derivative(reference_f, test_f, 10.0)
-check_derivative(reference_f, test_f, 2.0)
-check_derivative(reference_f, test_f, 1.0)
-check_derivative(reference_f, test_f, 0.0)
-
-function k_deriv_v1(n::Int64)
-  function(log_v1::Float64)
-    k_theta_deriv = exp(log_v1) * eye(n)
-    k_theta_deriv[10, 2]
-  end
-end
-
-function k_theta_v1(
-  y::Vector,
-  x::Matrix,
-  log_l2::Float64,
-  log_v0::Float64)
-  function(log_v1::Float64)
-    k_theta = kernel(x, x, sqrt(exp(log_l2)), exp(log_v0), exp(log_v1))
-    k_theta[10, 2]
-  end
-end
-
-reference_f = k_theta_v1(y, x, theta[3], theta[1])
-test_f = k_deriv_v1(length(y))
-check_derivative(reference_f, test_f, 10.0)
-check_derivative(reference_f, test_f, 2.0)
-check_derivative(reference_f, test_f, 1.0)
-check_derivative(reference_f, test_f, 0.0)
