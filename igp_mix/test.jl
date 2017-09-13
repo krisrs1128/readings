0eebd33eb1d0154f678c5e2d028b3d1a554b9252
Kris Sankaran <sankaran.kris@gmail.com>
2017-09-13 15:42:59 -0700

    need hyperparameters, and define @testset

--- igp_mix/test.jl
+++ igp_mix/test.jl
@@ -9,6 +9,7 @@
 # date: 08/29/2018
 
 using Base.Test
+using Calculus
 
 
 """Coordinatewise Projections of Kernel
@@ -85,6 +86,14 @@ function k_deriv_coords(x::Matrix,
   f_list
 end
 
+a = GPHyper(
+  Distributions.Logistic(-1, 4),
+  Distributions.Logistic(-1, 4),
+  Distributions.Logistic(-1, 4)
+)
+
+theta = rand_kernel(a)
+x, y = simulate(100, theta)
 coord_f = k_coords(x, theta)
 deriv_f = k_deriv_coords(x, theta)
 
@@ -96,29 +105,30 @@ deriv_f = k_deriv_coords(x, theta)
   end
 end
 
-## Check conditionals for gibbs sampler
-for i = 1:10
+@testset "Gibbs conditionals" begin
+  for i = 1:10
 
-  ## simulate toy data
-  n = 100
-  K = 3
-  c = rand(1:K, n)
-  update_ix = rand(1:n)
-  alpha = 2.0
-  thetas = Array{KernelParam}(K)
+    ## simulate toy data
+    n = 100
+    K = 3
+    c = rand(1:K, n)
+    update_ix = rand(1:n)
+    alpha = 2.0
+    thetas = Dict{Int64, KernelParam}()
 
-  for k = 1:K
-    thetas[k] = rand_kernel(a)
+    for k = 1:K
+      thetas[k] = rand_kernel(a)
+    end
+    c, x, y = simulate_mix(n, thetas)
+
+    ## swap one element
+    c_prime = deepcopy(c)
+    c_prime[update_ix] = rand(1:K)
+    joint_diff = joint_log_prob(c, x, y, thetas, alpha) -
+      joint_log_prob(c_prime, x, y, thetas, alpha)
+    conditional_probs = class_conditional(update_ix, c, x, y, thetas, alpha, a)[1]
+    conditional_diff = conditional_probs[c[update_ix]] -
+      conditional_probs[c_prime[update_ix]]
+    @test joint_diff - conditional_diff ≈ 0 atol = 1e-10
   end
-  c, x, y = simulate_mix(n, K, thetas)
-
-  ## swap one element
-  c_prime = deepcopy(c)
-  c_prime[update_ix] = rand(1:K)
-  joint_diff = joint_log_prob(c, x, y, thetas, alpha) -
-    joint_log_prob(c_prime, x, y, thetas, alpha)
-  conditional_probs = class_conditional(update_ix, c, x, y, thetas, alpha, a)[1]
-  conditional_diff = conditional_probs[c[update_ix]] -
-    conditional_probs[c_prime[update_ix]]
-  @test joint_diff - conditional_diff ≈ 0 atol = 1e-10
 end
