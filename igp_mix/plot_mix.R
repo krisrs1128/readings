6452b022955c54b8747002d0b98cddf39f30cbb6
Kris Sankaran <sankaran.kris@gmail.com>
2017-09-14 14:09:23 -0700

    c samples plot

--- igp_mix/plot_mix.R
+++ igp_mix/plot_mix.R
@@ -8,6 +8,7 @@
 ## date: 09/13/2017
 
 library("tidyverse")
+library("forcats")
 
 scale_colour_discrete <- function(...)
   scale_colour_brewer(..., palette="Set2")
@@ -28,22 +29,61 @@ min_theme <- theme_update(
   legend.key = element_blank()
 )
 
-post <- read_tsv("data/post_mix.csv", col_names = FALSE)
-data <- read_tsv("data/mix_data.csv", col_names = FALSE)
+preprocess_c <- function(c_samples) {
+  colnames(c_samples) <- c("iter", "sample", "class")
+  c_samples$truth <- as.factor(data$class[c_samples$sample])
+  c_samples$class <- factor(c_samples$class, levels = names(sort(table(c_samples$class), decreasing = TRUE)))
+  c_samples$class_group <- fct_lump(c_samples$class, n = 7)
+  c_samples$sample <- factor(c_samples$sample, levels = order(data$class))
+  c_samples
+}
 
+###############################################################################
+## Data generated from true mixture of GPs model
+###############################################################################
+post <- read_csv("data/mix_post.csv", col_names = FALSE)
+colnames(post) <- c("class", "x", "y")
+data <- read_csv("data/mix_data.csv", col_names = FALSE)
+colnames(data) <- c("class", "x", "y")
+
+## plot the raw data and the final estimated fits
 ggplot() +
   geom_line(
     data = post,
-    aes(x = X2, y = X3, group = X1),
+    aes(x = x, y = y, group = class),
   ) +
   geom_point(
     data = data,
-    aes(x = X1, y = X2)
-  ) +
-  ylim(-30, 10)
+    aes(x = x, y = y, col = as.factor(class))
+  )
 
-bump <- read_tsv("data/bump_data.csv", col_names = FALSE)
-post <- read_tsv("data/bump_posteriors.csv", col_names = FALSE)
+## read in the cluster membership data
+c_samples <- read_csv("data/samples/sim0914/c.csv", col_names = FALSE) %>%
+  preprocess_c()
+
+
+ggplot(c_samples) +
+  geom_tile(
+    aes(x = sample, y = iter, fill = class_group)
+  ) +
+  geom_text(
+    aes(x = sample, y = iter, label = class),
+    size = 0.75,
+    alpha = 0.5
+  ) +
+  scale_y_continuous(expand = c(0, 0)) +
+  scale_x_discrete(expand = c(0, 0)) +
+  facet_grid(. ~ truth, scale = "free", space = "free") +
+  theme(
+    legend.position = "none",
+    axis.text.x = element_blank(),
+    panel.border = element_rect(size = 0.5, fill = "transparent"),
+    panel.spacing = unit(0, "cm")
+  )
+
+## see how often samples were placed in the same cluster
+bump <- read_csv("data/bump_data.csv", col_names = FALSE)
+post <- read_csv("data/bump_posteriors.csv", col_names = FALSE)
 ggplot() +
   geom_point(
     data = bump,
