05173f86564616e8a798c6c485d6a443e9035996
Kris Sankaran <sankaran.kris@gmail.com>
2017-09-14 14:44:20 -0700

    create plot functions, to allow some code reuse

--- igp_mix/plot_mix.R
+++ igp_mix/plot_mix.R
@@ -68,6 +68,67 @@ coocurrence_counts <- function(x) {
   counts
 }
 
+## plot the raw data and the final estimated fits
+plot_fits <- function(data, post) {
+  ggplot() +
+    geom_line(
+      data = post,
+      aes(x = x, y = y, group = class),
+      ) +
+    geom_point(
+      data = data,
+      aes(x = x, y = y, col = as.factor(class))
+    )
+}
+
+## plot the class memberships over time
+plot_c <- function(c_samples) {
+  ggplot(c_samples) +
+    geom_tile(
+      aes(x = sample, y = iter, fill = class_group)
+    ) +
+    geom_text(
+      aes(x = sample, y = iter, label = class),
+      size = 0.75,
+      alpha = 0.5
+    ) +
+    scale_y_continuous(expand = c(0, 0)) +
+    scale_x_discrete(expand = c(0, 0)) +
+    facet_grid(. ~ truth, scale = "free", space = "free") +
+    theme(
+      legend.position = "none",
+      axis.text.x = element_blank(),
+      panel.border = element_rect(size = 0.5, fill = "transparent"),
+      panel.spacing = unit(0, "cm")
+    )
+}
+
+plot_coocurrence(counts) {
+  mcounts <- counts %>%
+    melt(
+      varnames = c("i", "j"),
+      value.name = "count"
+    ) %>%
+    mutate(
+      i = factor(i, levels = levels(c_samples$sample))
+      j = factor(j, levels = levels(c_samples$sample)),
+      class_group = factor(data$class[mcounts$i])
+    )
+
+  mcounts$i <- factor(mcounts$i, levels = levels(c_samples$sample))
+  mcounts$j <- factor(mcounts$j, levels = levels(c_samples$sample))
+  mcounts$class_group <- factor(data$class[mcounts$i])
+
+  ggplot(mcounts) +
+    geom_tile(
+      aes(
+        x = i,
+        y = j,
+        fill = class_group,
+        alpha = count)
+    ) +
+    scale_alpha(range = c(0, 1))
+}
 
 ###############################################################################
 ## Data generated from true mixture of GPs model
@@ -76,79 +137,18 @@ post <- read_csv("data/mix_post.csv", col_names = FALSE)
 colnames(post) <- c("class", "x", "y")
 data <- read_csv("data/mix_data.csv", col_names = FALSE)
 colnames(data) <- c("class", "x", "y")
-
-## plot the raw data and the final estimated fits
-ggplot() +
-  geom_line(
-    data = post,
-    aes(x = x, y = y, group = class),
-  ) +
-  geom_point(
-    data = data,
-    aes(x = x, y = y, col = as.factor(class))
-  )
+plot_fits(data, post)
 
 ## read in the cluster membership data
 c_samples <- read_csv("data/samples/sim0914/c.csv", col_names = FALSE) %>%
   preprocess_c()
-
-## plot the class memberships over time
-ggplot(c_samples) +
-  geom_tile(
-    aes(x = sample, y = iter, fill = class_group)
-  ) +
-  geom_text(
-    aes(x = sample, y = iter, label = class),
-    size = 0.75,
-    alpha = 0.5
-  ) +
-  scale_y_continuous(expand = c(0, 0)) +
-  scale_x_discrete(expand = c(0, 0)) +
-  facet_grid(. ~ truth, scale = "free", space = "free") +
-  theme(
-    legend.position = "none",
-    axis.text.x = element_blank(),
-    panel.border = element_rect(size = 0.5, fill = "transparent"),
-    panel.spacing = unit(0, "cm")
-  )
+plot_c(c_samples)
 
 ## plot co-occurrence of classes between samples
 counts <- coocurrence_counts(c_samples)
-
-mcounts <- counts %>%
-  melt(
-    varnames = c("i", "j"),
-    value.name = "count"
-  ) %>%
-  mutate(
-    i = factor(i, levels = levels(c_samples$sample))
-    j = factor(j, levels = levels(c_samples$sample)),
-    class_group = factor(data$class[mcounts$i])
-  )
-
-mcounts$i <- factor(mcounts$i, levels = levels(c_samples$sample))
-mcounts$j <- factor(mcounts$j, levels = levels(c_samples$sample))
-mcounts$class_group <- factor(data$class[mcounts$i])
-
-ggplot(mcounts) +
-  geom_tile(
-    aes(
-      x = i,
-      y = j,
-      fill = class_group,
-      alpha = count)
-  ) +
-  scale_alpha(range = c(0, 1))
+plot_coocurrence(counts)
 
 ## see how often samples were placed in the same cluster
 bump <- read_csv("data/bump_data.csv", col_names = FALSE)
 post <- read_csv("data/bump_posteriors.csv", col_names = FALSE)
-ggplot() +
-  geom_point(
-    data = bump,
-    aes(x = X1, y = X2)
-  ) +
-  geom_line(
-    data = post,
-    aes(x = X2, y = X3, group = X1)
-  )
+plot_fits(bump, post)
